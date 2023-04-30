a021e586c6f51a0d86f48205c7e032084f691df6
aliniluca <115418094+aliniluca@users.noreply.github.com>
2023-04-30 18:22:57 +0200

    Update DateDropdown.js

--- src/DateDropdown.js
+++ src/DateDropdown.js
@@ -26,7 +26,7 @@ const DateDropdown = ({ onSelect }) => {
         showYearDropdown
         dropdownMode="select"
         minDate={new Date('1900-01-01')}
-        maxDate={new Date('')}
+        maxDate={new Date(new Date().setDate(new Date().getDate()-365))}
         customInput={<FontAwesomeIcon icon={faCalendarAlt} />}
       />
     </div>
