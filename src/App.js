3c3722c672fbee9d6911ae109148d46e438eb950
aliniluca <115418094+aliniluca@users.noreply.github.com>
2023-04-30 15:50:02 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -9,31 +9,34 @@ import { faCalendarAlt } from "@fortawesome/free-solid-svg-icons";
 import './App.css';
 
 function App() {
-const TEXT_URL_1 = "https://drive.google.com/file/d/1dG-04EObi0EllBQPMZMm3bxgwCTHkKKv/view?usp=share_link";
-const TEXT_URL_2 = "https://drive.google.com/file/d/1g44ORRzhk9f8rIe_7MqSnwW1Rsv7qwwS/view?usp=share_link";
+//const TEXT_URL_1 = "https://drive.google.com/file/d/1dG-04EObi0EllBQPMZMm3bxgwCTHkKKv/view?usp=share_link";
+//const TEXT_URL_2 = "https://drive.google.com/file/d/1g44ORRzhk9f8rIe_7MqSnwW1Rsv7qwwS/view?usp=share_link";
 
-const [style1Text, setStyle1Text] = useState("");
-const [style2Text, setStyle2Text] = useState("");
+//const [style1Text, setStyle1Text] = useState("");
+//const [style2Text, setStyle2Text] = useState("");
 
-const fetchTextFromUrl = async (url) => {
-  const response = await fetch(url);
-  const text = await response.text();
-  return text;
-};
+//const fetchTextFromUrl = async (url) => {
+//  const response = await fetch(url);
+//  const text = await response.text();
+//  return text;
+//};
 
-const handleStyle1ButtonClick = async () => {
-  const text = await fetchTextFromUrl(TEXT_URL_1);
-  setStyle1Text(text);
-};
+//const handleStyle1ButtonClick = async () => {
+  //const text = await fetchTextFromUrl(TEXT_URL_1);
+  //setStyle1Text(text);
+//};
 
-const handleStyle2ButtonClick = async () => {
-  const text = await fetchTextFromUrl(TEXT_URL_2);
-  setStyle2Text(text);
-};
+//const handleStyle2ButtonClick = async () => {
+  //const text = await fetchTextFromUrl(TEXT_URL_2);
+  //setStyle2Text(text);
+//};
   const [birthdate, setBirthdate] = useState(null);
   const [gender, setGender] = useState(null);
   const [readingType, setReadingType] = useState(null);
   const [spintax, setSpintax] = useState('');
+  const style1 ="Set temperature to 4";
+  const style2 ="Set temperature to 4";
+  
 
   const generateSpintax = () => {
     if (!birthdate || !gender || !readingType) {
@@ -55,7 +58,7 @@ const handleStyle2ButtonClick = async () => {
 <button onClick={handleStyle2ButtonClick}>Style2</button>
       <GenerateButton onClick={generateSpintax} />
       
-      <div className="spintax-output">{spintax}+{style1Text}+{style2Text}</div>
+      <div className="spintax-output">{spintax}+{style1}+{style2}</div>
     </div>
   );
 }
