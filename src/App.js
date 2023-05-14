ad4d412d78a7e60ed4e5fc81921a3197e9c740c1
aliniluca <115418094+aliniluca@users.noreply.github.com>
2023-05-14 19:13:02 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -7,11 +7,7 @@ function App() {
   const [text, setText] = useState("");
   
 const AWS = require("aws-sdk");
-const s3 = new AWS.S3({
-  accessKeyId: process.env.AWS_ACCESS_KEY,
-  secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
-  region: process.env.AWS_REGION
-});
+const s3 = new AWS.S3()
   const handleButtonClick = async (number) => {
     if (!readingType) {
       alert('Please select a reading type');
